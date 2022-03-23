a94e3adc4e3fd30d6708db1db96a83b87fe78fbb
Michael Wiessner <michael-wiessner@web.de>
2022-03-23 12:53:54 +0100

    layout u. app /-blade status ok

--- resources/views/fahrer/index.blade.php
+++ resources/views/fahrer/index.blade.php
@@ -1,2 +1,11 @@
+@extends('layout.app')
 
-fahrer
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+            <h1 class="text-5xl uppercase bold">
+                fahrer
+            </h1>
+        </div>
+    </div>
+@endsection
